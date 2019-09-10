f1a0dff995164ea82a8a190135f7143d1b0267d6
Full Name <nickfuentes24@gmail.com>
2019-09-09 19:50:57 -0500

    Added another dispatcher to change the counter state by a certain value.

--- app.js
+++ app.js
@@ -9,11 +9,17 @@ const initialState = {
 
 // Create Reducer
 const reducer = (state = initialState, action) => {
-    console.log(action.type)
+    // console.log(action.type)
     if (action.type == "INC_COUNTER") {
         return {
             ...state, // copy the old state
             counter: state.counter + 1
+        }
+    } else if (action.type == "ADD_COUNTER") {
+        // console.log(action.value)
+        return {
+            ...state, // copy the old state
+            counter: state.counter + action.value
         }
     }
     return state
@@ -27,4 +33,8 @@ const store = createStore(reducer)
 store.dispatch({
     type: "INC_COUNTER"
 })
+console.log(store.getState())
+
+// Incremented the state by 50
+store.dispatch({ type: 'ADD_COUNTER', value: 50 })
 console.log(store.getState())
