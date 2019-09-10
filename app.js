7846dc15a9394c9f3213942c524c0b96be53689a
Full Name <nickfuentes24@gmail.com>
2019-09-09 19:28:42 -0500

    dispatch to store and checking to see type

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 const redux = require('redux')
 const createStore = redux.createStore
 
+// This is the intial state
+const initialState = {
+    counter: 0
+}
+
+// Create Reducer
+const reducer = (state = initialState, action) => {
+    console.log(action.type)
+}
+
+// Create Store
+const store = createStore(reducer)
 
 
+// Dispatch Actions
+store.dispatch({
+    type: "INC_COUNTER"
+})
+console.log(store.getState())
